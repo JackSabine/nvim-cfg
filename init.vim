f086ef68c8b9ed45873c42819d05b944c7c3e9eb
jsabine2 <jsabine2@jsabine2-lx01.engus1.analog.com>
2022-07-15 13:36:43 -0400

    "nvim from Scratch" on YT - options & keymaps
    Migrating init.vim to init.lua in dir "user".  Eventually will replace
    init.vim at the top with init.lua.

--- init.vim
+++ init.vim
@@ -35,65 +35,8 @@ call plug#begin()
 call plug#end()
 
 if has('nvim')
-  lua require('jsabine.init')
+  lua require('user.init')
 endif
-
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-"                                 Buffer sets                                  "
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-
-" Should be installed with the Vin Plug snippet above
-colorscheme codedark
-
-" Line numbers and columns
-set relativenumber
-set number
-set ruler
-
-" Better searching settings
-set hlsearch
-set incsearch
-set ignorecase
-set smartcase
-set magic
-set showmatch
-
-" Nobody likes bells...
-set noerrorbells
-set novisualbell
-set t_vb=
-
-set timeoutlen=300
-
-" Auto-newline at x chars
-set textwidth=100
-
-" Prefer to wrap lines instead of trail to the right
-set wrap
-
-set scrolloff=2
-
-set autoread
-
-set history=200
-
-set encoding=UTF-8
-
-set shell=sh
-
-" Highlight cursor line underneath the cursor horizontally.
-set cursorline
-
-" Configure backspace so it acts as it should act
-set backspace=eol,start,indent
-set whichwrap+=<,>,h,l
-set t_BE=
-
-" <TAB> -> 2 spaces
-set expandtab
-set tabstop=2
-set shiftwidth=2
-set softtabstop=2
 
 " Make :find recursive in :pwd
 set wildmenu
@@ -114,45 +57,11 @@ else
     set wildignore+=*/.git/*,*/.hg/*,*/.svn/*,*/.DS_Store
 endif
 
+colorscheme codedark
+
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 "                                Leader remaps                                 "
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-
-" O, Holy Comma!
-let g:mapleader=","
-
-" Two commas -> <Esc>
-imap <Leader>, <Esc>
-vmap <Leader>, <Esc>
-
-" Reloading vim config is easy!
-if has('nvim')
-  nmap <Leader>RR :so ~/.config/nvim/init.vim<CR>
-else
-  nmap <Leader>RR :so ~/.vimrc<CR> 
-endif
-
-" Copy/Paste to system register (X11 clipboard)
-nmap <Leader>c "+y
-nmap <Leader>v "+p
-nmap <Leader>V "+P
-
-" Paste from yank instead of volatile register
-nmap <Leader>p "0p
-nmap <Leader>P "0P
-
-nmap <Leader>xx :clo<CR>
-
-" Quick nohighlight mapping
-nmap <Leader>h :noh<CR>
-
-" Replace words with content of yank register
-nmap <Leader>rw diw"0P
-nmap <Leader>rW diW"0P
-
-" NERDTree binds
-nmap <Leader>t :NERDTreeToggle<CR>
-nmap <Leader>f :NERDTreeFind<CR>
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 "                               General remaps                                 "
@@ -168,22 +77,5 @@ function! NumberToggle()
 endfunction
 
 nnoremap <C-n> :call NumberToggle()<CR>
-
-" Add line above or below the cursor
-nmap oo o<Esc>
-nmap OO O<Esc>
-
-" Faster scrolling in normal and visual mode
-nnoremap <C-e> 4<C-e>
-nnoremap <C-y> 4<C-y>
-vnoremap <C-e> 4<C-e>
-vnoremap <C-y> 4<C-y>
-
-" Use regex characters whenever searching
-nnoremap / /\v
-
-" Center the pattern search in the buffer
-nnoremap n nzz
-nnoremap N Nzz
 
 noh
---
+++ lua/user/options.lua
@@ -0,0 +1,69 @@
+-- :help options
+local options = {
+  backup = false,                          -- creates a backup file
+  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
+  cmdheight = 1,                           -- space in the neovim command line for displaying messages
+  timeoutlen = 300,                        -- # ms between multi-key macros
+  textwidth = 100,                         -- auto-newline at x chars
+  wrap = true,                             -- prefer to wrap lines instead of trail to the right
+  scrolloff = 2,                           -- # lines above/below when scrolling
+  sidescrolloff = 2,                       -- # lines side to side when scrolling horizontally
+  autoread = true,                         -- update buffer with external changes
+  history = 200,                           -- save last 200 commands
+  encoding = "utf-8",                      -- internal encoding
+  fileencoding = "utf-8",                  -- encoding written to a file
+  shell = bash,                            -- shell used by :! commands
+  cursorline = true,                       -- highlight cursor line underneath the cursor horizontally
+  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+  conceallevel = 0,                        -- so that `` is visible in markdown files
+  mouse = "a",                             -- allow the mouse to be used in neovim
+  pumheight = 10,                          -- pop up menu height
+  showmode = true,                         -- show things like -- INSERT --
+  showtabline = 2,                         -- always show tabs
+  splitbelow = true,                       -- force all horizontal splits to go below current window
+  splitright = true,                       -- force all vertical splits to go to the right of current window
+  swapfile = true,                         -- creates a swapfile
+  undofile = true,                         -- enable persistent undo
+  writebackup = false,                     -- if a file is being edited by another program, it is not allowed to be edited
+  numberwidth = 4,                         -- set number line-number column width
+  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
+
+-- Line numbers and columns
+  relativenumber = true,                   
+  number = true,
+  ruler = true,
+
+-- Better searching settings
+  hlsearch = true,
+  incsearch = true,
+  ignorecase = true,
+  smartcase = true,
+  magic = true,
+  showmatch = true,
+
+-- Nobody likes bells...
+  errorbells = false,
+  visualbell = false,
+--  t_vb = "",
+
+
+-- configure backspace so it acts as it should act
+  backspace = eol,start,indent,
+--  t_BE= "",
+
+-- <TAB> -> 2 spaces
+  expandtab = true,
+  tabstop = 2,
+  shiftwidth = 2,
+  softtabstop = 2
+}
+
+vim.opt.shortmess:append "c"
+vim.opt.iskeyword:append "-"                    -- treat - as a keyword
+vim.opt.whichwrap:append "<,>,[,],h,l"
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+ 
+--vim.cmd "colorscheme=codedark"                  -- use vim-code-dark
