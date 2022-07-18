395cc006231f3036181f364b7bbbb9621bb6741e
Jack Sabine <jack@sabine.ws>
2022-07-18 07:21:23 -0400

    Add keymap comments and sync binds across modes

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -8,7 +8,7 @@ local keymap = vim.api.nvim_set_keymap
 -- Remap comma as leader key
 keymap("", ",", "<Nop>", opts)
 vim.g.mapleader = ","
-vim.g.maplocalleader = vim.g.mapleader
+vim.g.maplocalleader = ","
 
 -- Modes
 --   n: normal_mode
@@ -38,9 +38,9 @@ keymap("n", "<C-Right>",  ":vertical resize +2<CR>", opts)
 keymap("n", "<Leader>RR", ":so ~/.config/nvim/init.vim<CR>", opts)
 
 -- Copy/Paste to system register (X11 clipboard)
-keymap("n", "<Leader>c", "+y", opts)
-keymap("n", "<Leader>v", "+p", opts)
-keymap("n", "<Leader>V", "+P", opts)
+keymap("n", "<Leader>c", '"+y', opts)
+keymap("n", "<Leader>v", '"+p', opts)
+keymap("n", "<Leader>V", '"+P', opts)
 
 -- Paste from yank instead of volatile register
 keymap("n", "<Leader>p", "\"0p", opts)
@@ -60,11 +60,15 @@ keymap("n", "<Leader>rW", "diW\"0P", opts)
 keymap("n", "<Leader>t", ":NERDTreeToggle<CR>", opts)
 keymap("n", "<Leader>f", ":NERDTreeFind<CR>", opts)
 
+-- Switch between tabs
+keymap("n", "<Leader>.", ":tabp<CR>", opts)
+keymap("n", "<Leader>/", ":tabn<CR>", opts)
+
 -- Add line above or below the cursor
 keymap("n", "oo", "o<Esc>", opts)
 keymap("n", "OO", "O<Esc>", opts)
 
--- Faster scrolling in normal and visual mode
+-- Faster scrolling in normal
 keymap("n", "<C-e>", "4<C-e>", opts)
 keymap("n", "<C-y>", "4<C-y>", opts)
 
@@ -79,14 +83,17 @@ keymap("n", "N", "Nzz", opts)
 --    Insert    --
 ------------------
 
+-- Two commas -> <Esc>
 keymap("i", "<Leader>,", "<Esc>", opts)
 
 ------------------
 --    Visual    --
 ------------------
 
+-- Two commas -> <Esc>
 keymap("v", "<Leader>,", "<Esc>", opts)
 
+-- Faster scrolling in visual
 keymap("v", "<C-e>", "4<C-e>", opts)
 keymap("v", "<C-y>", "4<C-y>", opts)
 
@@ -111,9 +118,21 @@ keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
--- Terminal --
+-- Paste from yank instead of volatile register
+keymap("x", "<Leader>p", '"0p', opts)
+keymap("x", "<Leader>P", '"0P', opts)
+
+-- Faster scrolling in visual block
+keymap("x", "<C-e>", "4<C-e>", opts)
+keymap("x", "<C-y>", "4<C-y>", opts)
+
+------------------
+--   Terminal   --
+------------------
+
 -- Better terminal navigation
 keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
