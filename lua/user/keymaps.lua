e364ece531e7a395dcd3b7b453882d7fec97a6bf
Jack Sabine <jack@sabine.ws>
2022-12-30 00:30:52 -0500

    Change: move gitsigns keymaps to keymaps.lua

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -175,3 +175,43 @@ end
 function Set_Format_Keymap(bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<M-f>", "<cmd>lua vim.lsp.buf.format({async=true})<CR>", opts)
 end
+
+function Set_Gitsigns_Keyamps(bufnr)
+  local gs = package.loaded.gitsigns
+
+  local function map(mode, l, r, options)
+    options = options or {}
+    options.buffer = bufnr
+    vim.keymap.set(mode, l, r, options)
+  end
+
+  -- Navigation
+  map('n', ']c', function()
+    if vim.wo.diff then return ']c' end
+    vim.schedule(function() gs.next_hunk() end)
+    return '<Ignore>'
+  end, { expr = true })
+
+  map('n', '[c', function()
+    if vim.wo.diff then return '[c' end
+    vim.schedule(function() gs.prev_hunk() end)
+    return '<Ignore>'
+  end, { expr = true })
+
+  -- Actions
+  -- map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+  -- map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+  -- map('n', '<leader>hS', gs.stage_buffer)
+  -- map('n', '<leader>hu', gs.undo_stage_hunk)
+  -- map('n', '<leader>hR', gs.reset_buffer)
+  map('n', '<leader>hp', gs.preview_hunk)
+  map('n', '<leader>hb', function() gs.blame_line { full = false } end)
+  map('n', '<leader>hB', function() gs.blame_line { full = true } end)
+  -- map('n', '<leader>tb', gs.toggle_current_line_blame)
+  map('n', '<leader>hd', gs.diffthis)
+  map('n', '<leader>hD', function() gs.diffthis('~') end)
+  -- map('n', '<leader>td', gs.toggle_deleted)
+
+  -- Text object
+  map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+end
