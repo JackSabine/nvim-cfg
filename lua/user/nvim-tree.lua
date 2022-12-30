2e47eac8bb6faab19212bff553149cbe1b39d13d
Jack Sabine <jack@sabine.ws>
2022-12-29 23:54:27 -0500

    Add: nvim-tree nerdfont and keymaps settings

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -3,6 +3,13 @@ if not status_ok then
   return
 end
 
+local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+if not config_status_ok then
+  return
+end
+
+local tree_cb = nvim_tree_config.nvim_tree_callback
+
 nvimtree.setup({
   sort_by = "case_sensitive",
   view = {
@@ -10,15 +17,60 @@ nvimtree.setup({
     side = "left",
     mappings = {
       list = {
+        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+        { key = "h", cb = tree_cb "close_node" },
+        -- { key = "v", cb = tree_cb "vsplit" },
+        { key = { "<Esc>", "q" }, cb = tree_cb "close" },
         { key = "u", action = "dir_up" },
       },
     },
   },
-  renderer = {
-    group_empty = true,
-  },
   filters = {
     dotfiles = true,
+  },
+
+  update_focused_file = {
+    enable = true,
+    update_root = true,
+  },
+  renderer = {
+    group_empty = true,
+    root_folder_modifier = ":t",
+    icons = {
+      glyphs = {
+        default = "",
+        symlink = "",
+        folder = {
+          arrow_open = "",
+          arrow_closed = "",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
+        },
+        git = {
+          unstaged = "M",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "U",
+          deleted = "D",
+          ignored = "◌",
+        },
+      },
+    },
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = false,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    },
   },
 })
 
