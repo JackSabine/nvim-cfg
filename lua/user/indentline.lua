a6c3c3a7279710a2564ffd883dfbf9cf08c0d4f1
Jack Sabine <jack@sabine.ws>
2024-07-05 16:10:12 -0400

    Fix(indentblankline): update setup config to latest

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -5,9 +5,23 @@ end
 
 indent_blankline.setup({
   enabled = true,
+  debounce = 100,
   indent = { char = "▏" },
   scope = {
+    enabled = true,
+    char = "▏",
     show_start = true,
-    show_end = true,
+    show_end = false,
+    exclude = {
+      language = {
+        "help",
+        "startify",
+        "dashboard",
+        "packer",
+        "neogitstatus",
+        "NvimTree",
+        "Trouble",
+      },
+    },
   },
 })
